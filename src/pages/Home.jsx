0d2428bffaf35edffa6742fc59854f06faf8a1a0
abhilash-dev-glitch <92076687+abhilash-dev-glitch@users.noreply.github.com>
2025-08-02 21:54:20 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import heroImage from './assets/collage.png';
+import heroImage from 'src/assets/collage.png';
 
 const Home = () => {
   return (
